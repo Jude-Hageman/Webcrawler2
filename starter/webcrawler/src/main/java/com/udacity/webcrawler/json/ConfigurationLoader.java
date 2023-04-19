85faa148c119a8b757034a5e292ab59e7febdaf5
Jude-Hageman <131105266+Jude-Hageman@users.noreply.github.com>
2023-04-18 20:25:00 -0400

    Update ConfigurationLoader.java

--- starter/webcrawler/src/main/java/com/udacity/webcrawler/json/ConfigurationLoader.java
+++ starter/webcrawler/src/main/java/com/udacity/webcrawler/json/ConfigurationLoader.java
@@ -1,14 +1,24 @@
 package com.udacity.webcrawler.json;
 
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.Reader;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Objects;
+
 
 /**
  * A static utility class that loads a JSON configuration file.
  */
+@JsonDeserialize(builder = CrawlerConfiguration.Builder.class)
 public final class ConfigurationLoader {
 
+  private static Object CrawlerConfiguration;
   private final Path path;
 
   /**
@@ -23,10 +33,19 @@ public final class ConfigurationLoader {
    *
    * @return the loaded {@link CrawlerConfiguration}.
    */
+
   public CrawlerConfiguration load() {
     // TODO: Fill in this method.
-
-    return new CrawlerConfiguration.Builder().build();
+    CrawlerConfiguration crawlerConfiguration = null;
+    ObjectMapper objectMapper = new ObjectMapper();
+    try {
+      Reader reader = Files.newBufferedReader(path);
+      crawlerConfiguration = ConfigurationLoader.read(reader);
+      reader.close();
+    } catch (NullPointerException | IOException e) {
+      e.printStackTrace();
+    }
+    return crawlerConfiguration;
   }
 
   /**
@@ -39,7 +58,18 @@ public final class ConfigurationLoader {
     // This is here to get rid of the unused variable warning.
     Objects.requireNonNull(reader);
     // TODO: Fill in this method
-
-    return new CrawlerConfiguration.Builder().build();
+    ObjectMapper objectMapper = new ObjectMapper();
+    CrawlerConfiguration crawlerConfiguration = null;
+    JsonParser.Feature Feature = JsonParser.Feature.AUTO_CLOSE_SOURCE;
+    try {
+      objectMapper.disable(Feature);
+      crawlerConfiguration = (CrawlerConfiguration)
+      objectMapper.readValue(reader, CrawlerConfiguration.class);
+    } catch (NullPointerException e) {
+      e.printStackTrace();
+    } finally {
+      return crawlerConfiguration;
+    }
   }
 }
+
