3a74c2aa15311745f15a67ea65f42a108d5181d3
Jude-Hageman <131105266+Jude-Hageman@users.noreply.github.com>
2023-04-23 19:32:33 -0400

    Update ProfilingMethodInterceptor.java

--- starter/webcrawler/src/main/java/com/udacity/webcrawler/profiler/ProfilingMethodInterceptor.java
+++ starter/webcrawler/src/main/java/com/udacity/webcrawler/profiler/ProfilingMethodInterceptor.java
@@ -3,7 +3,8 @@ package com.udacity.webcrawler.profiler;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.time.Clock;
-import java.util.Objects;
+import java.time.Duration;
+import java.time.Instant;
 
 /**
  * A method interceptor that checks whether {@link Method}s are annotated with the {@link Profiled}
@@ -13,18 +14,24 @@ final class ProfilingMethodInterceptor implements InvocationHandler {
 
   private final Clock clock;
 
-  // TODO: You will need to add more instance fields and constructor arguments to this class.
+
   ProfilingMethodInterceptor(Clock clock) {
-    this.clock = Objects.requireNonNull(clock);
+    this.clock = clock;
   }
 
+
   @Override
-  public Object invoke(Object proxy, Method method, Object[] args) {
-    // TODO: This method interceptor should inspect the called method to see if it is a profiled
-    //       method. For profiled methods, the interceptor should record the start time, then
-    //       invoke the method using the object that is being profiled. Finally, for profiled
-    //       methods, the interceptor should record how long the method call took, using the
-    //       ProfilingState methods.
-    return null;
+  public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+    Profiled annotation = method.getAnnotation(Profiled.class);
+    if (annotation != null) {
+      Instant start = clock.instant();
+      Object result = method.invoke(proxy, args);
+      Instant end = clock.instant();
+      Duration duration = Duration.between(start, end);
+      ProfilingState.record(annotation.getClass(), duration);
+      return result;
+    } else {
+      return method.invoke(proxy, args);
+    }
   }
 }
